050417b0afd1ad058c902cea98f1764b9eb45b81
Gera <go47455@gmail.com>
2025-05-24 13:16:21 -0600

    diseño de calendario

--- views/tareas.php
+++ views/tareas.php
@@ -21,7 +21,7 @@ $stmt->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/leveling/assets/css/styles.css?v=1.0">
-    <link rel="stylesheet" href="/leveling/assets/css/tareas.css?v=1.0">
+    <link rel="stylesheet" href="/leveling/assets/css/tareas.css?v=1.1">
     <title>Mis Tareas - Solo Leveling</title>
     <style>
         .task-mark{
